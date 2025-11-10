2af49810f303c7fb930692ccec84a51d5a1f72ac
Krutifulmen <143906543+Krutifulmen@users.noreply.github.com>
2025-11-10 19:08:13 +0300

    Update history_view_theme_document.cpp

--- Telegram/SourceFiles/history/view/media/history_view_theme_document.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_theme_document.cpp
@@ -664,7 +664,7 @@ rpl::producer<QString> GiftThemeBox::button() {
 
 ClickHandlerPtr GiftThemeBox::createViewLink() {
 	return std::make_shared<UrlClickHandler>(
-		u"otg://nft?slug="_q + _data.unique->slug);
+		u"dg://nft?slug="_q + _data.unique->slug);
 }
 
 int GiftThemeBox::buttonSkip() {
